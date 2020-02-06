bb24422fc18d96529a492026a92076577cdec0bb
Pablo Lopez Santana <esviasalpa@mail.com>
2020-02-06 14:40:30 +0100

    Realizando  ejercico 2 de ORM provincias

--- Hibernate/ORM_Provincias/src/orm_provincias/ORM_Provincias.java
+++ Hibernate/ORM_Provincias/src/orm_provincias/ORM_Provincias.java
@@ -5,6 +5,9 @@
  */
 package orm_provincias;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author alumno1920
@@ -16,6 +19,21 @@ public class ORM_Provincias {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        try ( Session s = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction t = null;
+
+            try {
+                t = s.beginTransaction();
+
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+                if (t != null) {
+                    t.rollback();
+                }
+            }
+
+        }
     }
-    
+
 }
